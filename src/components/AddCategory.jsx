8dc70e37992ef984183f8a8f7ebc03d18dd6f35f
Jorge Jimenez <xadrijo@gmail.com>
2022-12-03 19:46:08 -0300

    first commit

---
+++ src/components/AddCategory.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react"
+
+export const AddCategory = ({ onNewCategory }) => {
+
+    const [inputValue, setinputValue] = useState('')
+
+    const onInputChange = ({ target }) => {
+        setinputValue( target.value )
+    }
+
+    const onSubmit = (event) => {
+        event.preventDefault()
+        if( inputValue.trim().length <= 1 ) return;
+
+        onNewCategory( inputValue.trim() )
+        setinputValue('')
+    }
+
+    return (
+        <form onSubmit={ onSubmit }>
+            <input 
+                type="text"
+                placeholder="Search gifs"
+                value={ inputValue }
+                onChange={ onInputChange }
+            />
+        </form>
+    )
+}
---
+++ src/helpers/getGifs.js
@@ -0,0 +1,13 @@
+export const getGifs = async( category ) => {
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=i4z2U0YjCZMEhBKS2K0YyGRzRFGCdfGj&q=${ category }&limit=20`
+    const resp = await fetch( url )
+    const { data } = await resp.json()
+
+    const gifs = data.map( img => ({
+        id: img.id,
+        title: img.title,
+        url: img.images.downsized_medium.url
+    }))
+
+    return gifs
+}
