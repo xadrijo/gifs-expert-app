03e0f19a4c7f16bb2fa2e93d5b2ebc5767d13f62
xadrijo <xadrijo@gmail.com>
2022-12-04 11:20:28 -0300

    GifItem's unit test

---
+++ tests/GifItem.test.jsx
@@ -0,0 +1,27 @@
+import { render, screen } from "@testing-library/react"
+import { GifItem } from "../src/components/GifItem"
+
+describe('<GifItem />', () => {
+
+    const title = 'Goku'
+    const url = 'https://goku.com/goku.jpg'
+
+    test('should match with snapshot', () => {
+        const { container } = render( <GifItem title={ title } url={ url } /> )
+        expect( container ).toMatchSnapshot()
+    })
+
+    test('should show image with URL and ALT', () => {
+        render( <GifItem title={ title } url={ url } /> )
+
+        const { src, alt } = screen.getByRole('img')
+        expect( src ).toBe( url )
+        expect( alt ).toBe( alt )
+    })
+    
+    test("should show component's title", () => {
+        render( <GifItem title={ title } url={ url } /> )
+        expect( screen.getByText( title )).toBeTruthy()
+    })
+    
+})
